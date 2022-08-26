4ce86b5fda4133a42d0ddca6507344eaa64af0ec
Rohit Singh Katiyar <rohitsk@tech-radix.com>
2022-08-26 19:54:00 +0530

    Applied aoi

--- src/Components/ForgotPassword .js
+++ src/Components/ForgotPassword .js
@@ -1,14 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from "../../src/assests/Logo.png"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 export default function ForgotPassword() {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState("")
+  const [loading, setLoading] = useState(false)
+  const handleSubmit = () => {
+    setLoading(true)
+    let url = process.env.REACT_APP_BASEURL + `api/password_reset/`
+    const config = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-type": "application/json"
+      }
+    }
+    axios.post(url, email, config)
+      .then(res => {
+        console.log(res);
+        if (res.code == 200) {
+          setLoading(false)
+          navigate('/reset-password')
+          console.log(res.message);
+        }
+        if (res.code == 400) {
+          setLoading(false)
+          console.log(res.errors);
+        }
+        else {
+          console.log(res.code);
+        }
+      }
+      )
+      .catch(err => {
+        setLoading(false)
+        console.log(err);
+      })
+  }
   return (
     <div>
+      {JSON.stringify(email)}
       <div className='container-fluid form' >
         <div className="row ">
           <div className="col-md-4 col-lg-4 "></div>
           <div className="col-md-4 mt-1 pt-5 pb-5 mb-5 ">
-            <img src={Logo} className="text-center rounded mx-auto mb-1 d-block img-fluid" alt=''/>
+            <img src={Logo} className="text-center rounded mx-auto mb-1 d-block img-fluid" alt='' />
             <h4 className='text-center b3black'>Welcome to Human Resources key</h4>
             <div className="card text-center d-flex form-body ">
               <div className="card-body text-center">
@@ -19,12 +55,17 @@ export default function ForgotPassword() {
                     <label className="w-100 mb-2 mt-1 text-start c2medium">Email Address  </label>
                     <div className="input-group mb-3">
                       <span id="inputGroup-sizing-default"></span>
-                      <input type="email" className="form-control form-input" placeholder='Email ID' aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                      <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="email" className="form-control form-input" placeholder='Email ID' aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
                     </div>
                   </div>
 
-
-                  <button type="button" className="btn w-100 col-10 mt-3 form-btn" > Reset</button>
+                  {loading ?
+                    <button type="button" className="btn w-100 col-10 mt-3 form-btn" onClick={handleSubmit} >
+                      Sending... <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                    </button>
+                    :
+                    <button type="button" className="btn w-100 col-10 mt-3 form-btn" onClick={handleSubmit} > Reset</button>
+                  }
                   <div className="row mt-3" style={{ fontSize: '14px', fontWeight: '450', lineHeight: '18px', letterSpacing: '0em' }}>
                     <p className=' small m-1 text-center c2book'>Remember your Password? <Link to="/login" style={{ color: "#FC6075" }} className='text-decoration-none'>Login</Link></p>
                   </div>
