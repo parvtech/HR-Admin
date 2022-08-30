2228701161454df5951eed6b8585481605aa7e60
Aashivyas <aashitechradix@gmail.com>
2022-08-30 18:18:09 +0530

    tacke rebase and resolve conflict.

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Profileimg from '../assests/img/Rectangle.png'
 
-import { MdCancel} from "react-icons/md"
+import { MdCancel } from "react-icons/md"
 
- const ProfileInfo = ()=> {
+const ProfileInfo = () => {
     return (
         <>
             <div className='container'>
@@ -13,7 +13,7 @@ import { MdCancel} from "react-icons/md"
                         <div className="card " style={{ width: "800px", height: "500px" }}>
                             <div className="text-center mb-0 ">
                                 <div className="text-end me-2">
-                                <span style={{ color: "gray", borderLeft: "none", marginTop: "4px", borderTopRightRadius: "5px", borderBottomRightRadius: "5px", borderRightt: "none" }} id="basic-addon1"><MdCancel/></span>
+                                    <span style={{ color: "gray", borderLeft: "none", marginTop: "4px", borderTopRightRadius: "5px", borderBottomRightRadius: "5px", borderRightt: "none" }} id="basic-addon1"><MdCancel /></span>
                                 </div>
                                 <p className='h3medium'>Profile Information</p>
                                 <img alt='' src={Profileimg} style={{ width: "100px", height: "100px" }} className="mt-1" />
@@ -21,34 +21,34 @@ import { MdCancel} from "react-icons/md"
                             <div className='container'>
                                 <div className="row mt-4">
                                     <div className='col-6'>
-                                        <div class="input-group mb-1">
-                                            <input type="text" class="form-control" placeholder="Full Name*" aria-label="Username" aria-describedby="basic-addon1" required />
+                                        <div className="input-group mb-1">
+                                            <input type="text" className="form-control" placeholder="Full Name*" aria-label="Username" aria-describedby="basic-addon1" required />
                                         </div>
                                     </div>
                                     <div className='col-6'>
-                                        <div class="input-group mb-1">
-                                            <input type="text" class="form-control" placeholder="Employee ID*" aria-label="Username" aria-describedby="basic-addon1" required />
+                                        <div className="input-group mb-1">
+                                            <input type="text" className="form-control" placeholder="Employee ID*" aria-label="Username" aria-describedby="basic-addon1" required />
                                         </div>
                                     </div>
                                 </div>
                                 <div className="row mt-1">
                                     <div className='col-6'>
-                                        <div class="input-group mb-1">
-                                            <input type="date" class="form-control" placeholder="Birthday*" aria-label="Username" aria-describedby="basic-addon1" required />
+                                        <div className="input-group mb-1">
+                                            <input type="date" className="form-control" placeholder="Birthday*" aria-label="Username" aria-describedby="basic-addon1" required />
                                         </div>
                                     </div>
                                     <div className='col-6'>
-                                        <select class="form-select" aria-label="Default select example">
+                                        <select className="form-select" aria-label="Default select example">
                                             <option selected>Male</option>
                                             <option value="1">Female</option>
                                             <option value="2"></option>
-                                            
+
                                         </select>
                                     </div>
                                 </div>
                                 <div className="row mt-1">
                                     <div className='col-6'>
-                                        <select class="form-select" aria-label="Default select example">
+                                        <select className="form-select" aria-label="Default select example">
                                             <option selected>Department*</option>
                                             <option value="1">One</option>
                                             <option value="2">Two</option>
@@ -56,26 +56,26 @@ import { MdCancel} from "react-icons/md"
                                         </select>
                                     </div>
                                     <div className='col-6'>
-                                        <div class="input-group mb-1">
-                                            <input type="text" class="form-control" placeholder="Email Address*" aria-label="Username" aria-describedby="basic-addon1" required />
+                                        <div className="input-group mb-1">
+                                            <input type="text" className="form-control" placeholder="Email Address*" aria-label="Username" aria-describedby="basic-addon1" required />
                                         </div>
                                     </div>
                                 </div>
                                 <div className="row mt-1">
                                     <div className='col-6'>
-                                        <div class="input-group mb-1">
-                                            <input type="text" class="form-control" placeholder="Joining Date" aria-label="Username" aria-describedby="basic-addon1" />
+                                        <div className="input-group mb-1">
+                                            <input type="text" className="form-control" placeholder="Joining Date" aria-label="Username" aria-describedby="basic-addon1" />
                                         </div>
                                     </div>
                                     <div className='col-6'>
-                                        <div class="input-group mb-1">
-                                            <input type="text" class="form-control" placeholder="Phone Number*" aria-label="Username" aria-describedby="basic-addon1" required />
+                                        <div className="input-group mb-1">
+                                            <input type="text" className="form-control" placeholder="Phone Number*" aria-label="Username" aria-describedby="basic-addon1" required />
                                         </div>
                                     </div>
                                 </div>
                                 <div className="row mt-1">
                                     <div className='col-6'>
-                                        <select class="form-select" aria-label="Default select example">
+                                        <select className="form-select" aria-label="Default select example">
                                             <option selected className=''>Select Designation</option>
                                             <option value="1">One</option>
                                             <option value="2">Two</option>
@@ -90,7 +90,7 @@ import { MdCancel} from "react-icons/md"
                             <div className='container'>
                                 <div className='row'>
                                     <div className='col text-center mt-4'>
-                                    <button type="button" class="btn rounded-pill w-25 text-white" style={{background:"#FF9B44",border:'#FF9B44'}}>Secondary</button>
+                                        <button type="button" className="btn rounded-pill w-25 text-white" style={{ background: "#FF9B44", border: '#FF9B44' }}>Secondary</button>
                                     </div>
                                 </div>
                             </div>
@@ -105,4 +105,4 @@ import { MdCancel} from "react-icons/md"
 
     )
 }
-export default  ProfileInfo
+export default ProfileInfo
--- src/components/employee/Employee.js
+++ src/components/employee/Employee.js
@@ -1,9 +1,46 @@
-import React from 'react'
-import "../employee/Employee.css"
-import { GoPlus } from 'react-icons/go'
-import Card from '../Card'
-
+import React, { useState, useEffect } from 'react'
+import "./Employee.css"
+import Svg from '../../assests/img/Group.svg'
+import axios from 'axios'
+import { useNavigate, } from 'react-router-dom'
+import { GoAlert, GoPlus } from 'react-icons/go'
+import Rectangle from '../../assests/img/Rectangle.png'
 export default function Employee() {
+
+
+  const [employeedetail, setEmployeedetail] = useState([]);
+  const [image, setImage] = useState(null)
+  let navigate = useNavigate()
+  useEffect(() => {
+    getEmployeList()
+  }, []);
+
+
+  // fetch api
+  const getEmployeList = () => {
+    let url = process.env.REACT_APP_BASEURL + "employee/"
+    const config = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-type": "application/json",
+        "Authorization": "Bearer " + '6160b016c6c20ad11a915fc97a6f21d4a252a408acfbfb30f245be0e560da6764942c23da7a6b64c1cb37907def338621af493cee41ceaa2b619dad37324b5a0ebed8fd75f0a2ed44204b56ebbd8833f18010044f2f8683'
+      }
+    };
+    axios.get(url, config)
+      .then(res => {
+        setEmployeedetail(res.data.data)
+      }
+      ).catch(err => {
+
+      })
+  }
+  // onclick function
+  const profileDetail = (public_id) => {
+    console.log(public_id)
+    navigate(`/profile/${public_id}`)
+
+  }
+
 
   return (
     <div>
@@ -20,9 +57,9 @@ export default function Employee() {
           <div className='col input-group emprow'>
             <input type="email" className="form-control" placeholder='Employee Name' aria-label="Sizing example " />
           </div>
-          <div class="col input-group emprow">
-            <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
-              <option selected>Select Designation</option>
+          <div className="col input-group emprow">
+            <select className="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+              <option>Select Designation</option>
               <option value="1">...</option>
               <option value="2">...</option>
               <option value="3">...</option>
@@ -31,7 +68,37 @@ export default function Employee() {
           <div className='col-3 '><button className="form-control empbtn text-white ">SEARCH</button></div>
         </div>
       </div>
-      <Card />
+
+      <div className='container-fluid'>
+        <div className='row'>
+          {
+            employeedetail.map((item) => (
+
+              <div key={item.public_id} onClick={() => profileDetail(item.public_id)} className='col-sm-6 col-md-3 mt-4 d-flex justify-content-center'>
+                <div className="card " style={{ width: "19.4rem" }}>
+                  <div className="text-center  ">
+                    <div className="text-end me-2 pe-2 pt-2">
+                      <img alt='' src={Svg} />
+                    </div>
+                    {/* <img alt='' src={Rectangle} style={{ width: "70px" }} /> */}
+                    {(item.avatar == '' || item.avatar == null) ?
+                      <img src={require("../../assests/avatar.png")} alt="logo" width="70px" height="60px" className='mt-2 mr-2' style={{ borderRadius: "50%" }}></img> :
+                      <img src={item.avatar} alt="logo" width="70px" height="60px" className='mt-3 mr-1' style={{ borderRadius: "50%" }}></img>
+                    }
+
+                  </div>
+                  <div className="card-body">
+                    <p className="card-text text-center mb-0 card-img-head-width text-content b2medium">{item.fullname ? item.fullname : 'N/A'}</p>
+                    <p className="card-text text-center card-img-subhead text-gry ">{item.designation ? item.designation : 'N/A'}</p>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+
+        </div >
+
+      </div >
     </div>
   )
 }
