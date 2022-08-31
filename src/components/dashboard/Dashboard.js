6d7a75d164f186c335ef230f5097f9add8813d7b
Rohit Singh Katiyar <rohitsk@tech-radix.com>
2022-08-31 16:22:08 +0530

    Working on routing

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 const Dashboard = () => {
   return (
-<div className='d-flex justify-content-center'>
-     <div>
-       <h3>Dashboardddddddddddddddddddddddddd</h3>
+    <div className='d-flex justify-content-center h-full w-full'>
+      <div>
+        <h3>Dashboard</h3>
+      </div>
     </div>
-</div>
   )
 }
 
